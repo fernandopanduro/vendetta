e27593c16708c263ba6c5abbcd1b0230e060f18e
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-01-23 13:12:04 -0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { Header } from "./components/header";
 
 import styles from './assets/css/app.module.css';
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import { LandingPage } from "./pages/landinPage";
+import { Navigater } from "./components/navigate";
 
 function App() {
   return (
-    <div className={styles.app}>
+    <div >
       <Header />
+
+      <main className={styles.app}>
+
+      <Navigater />
+
+        {/* <Routes>
+          <Route path='/' element={<LandingPage />} />
+          
+          <Route path='*' element={<Navigate replace to='/' />} />
+        </Routes> */}
+
+      </main>
+
     </div>
   );
 }
