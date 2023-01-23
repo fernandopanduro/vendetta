d18eefd3bddf8fd40366d80f9f0cd4131030e105
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-01-23 11:42:49 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Header } from "./components/header";
 
+import styles from './assets/css/app.module.css';
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className={styles.app}>
+      <Header />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
+
+import './assets/css/index.css';
+import './assets/css/normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
